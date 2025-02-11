bb458ab3b1a268b8a3836f71e50da907e7f563e4
Nmongkol <mongkol15099@gmail.com>
2025-02-11 13:57:10 +0700

    pro18.1

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,7 +5,10 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect A1,Rect B2){
+    double width = max(0.0, min(A1.x + A1.w, B2.x + B2.w) - max(A1.x, B2.x));
+    double height = max(0.0, min(A1.y, B2.y) - max(A1.y - A1.h, B2.y - B2.h));
+    return width * height;
 
 
 
