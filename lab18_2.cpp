58915b9ea957253de2e3359f247863501bf5a807
Nmongkol <mongkol15099@gmail.com>
2025-02-11 13:57:58 +0700

    pro18.2

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 struct Rect{
-	double x,y,w,h;
+    double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *A1, Rect *B2){
+    double width = max(0.0, min(A1->x + A1->w, B2->x + B2->w) - max(A1->x, B2->x));
+    double height = max(0.0, min(A1->y, B2->y) - max(A1->y - A1->h, B2->y - B2->h));
+    return width * height;
 }
